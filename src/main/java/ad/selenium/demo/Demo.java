ef784b92af54e61439f083c4fca16072a177afff
PoojaMSunil <Poojamalle60@gmail.com>
2025-01-04 07:58:56 +0530

    push1

---
+++ src/main/java/ad/selenium/demo/Demo.java
@@ -0,0 +1,24 @@
+package ad.selenium.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Demo {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.makemytrip.com/");
+		Actions a = new Actions(driver);
+		a.moveByOffset(10,20).click().perform();
+		Thread.sleep(2000);
+		driver.findElement(By.className("react-autosuggest__input react-autosuggest__input--open")).click();
+		driver.findElement(By.xpath("//p[text()='Pune, India']")).click();
+		System.out.println("selected pune");
+		driver.findElement(By.xpath("//span[text()='Round Trip']")).click();
+		
+		System.out.println("selected");
+		driver.close();
+	}
+}
